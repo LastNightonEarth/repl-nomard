33f81b92a424a7f918e8cd424687497f16e609a3
Nacho <lip7280@naver.com>
2021-03-04 04:32:59 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,58 @@
+/* 2021-03-02
+console.log('Im Working. Im JS. Im so Beautiful');
+alert('Im Working. Im JS. Im so Beautiful'); 
+
+let a = 221;
+b = a - 5
+console.log(c)
+*/
+// 한줄주석
+/* 복수 주석
+ String 
+  const what = "nacho";
+  const what = "010-9574-1911"; 
+  console.log(what);*/
+/* Boolean
+Boolean is true or false
+const wat = false;
+const wat = true;
+모두 소문자로 사용 ("" ) 사용하지않음 text X
+2진법 바이너리 
+0 or 1
+Number
+const wat =4444;
+console.log(wat);
+*/
+
+/*Float
+const wat = 55.1;
+console.log (wat);
+
+*/
+/*camel case 
+ (. /) 변수앞에 사용할수없음 
+
+   const monday = "Mon";
+  const tue= "Tue";
+  const wed = "Wed";
+  const thu = "Thu";
+  const fri = "Fri";
+  
+  console.log(monday ,tue, wed , thu , fri);
+ 
+ Array는 여러 String들을 하나로 묶음
+   const daysOfWeek = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun",true];
+
+  console.log(daysOfWeek);
+ 
+ console.log(daysOfWeek[2]);
+ 컴퓨터는 0,1,2 (0부터 숫자를 세기때문에)
+ [2] 시 Wed 값이나옴.
+ 
+     const something = "Something"
+
+     const daysOfWeek = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun",54,true,"stuff","legend", something];
+
+  console.log(daysOfWeek); 
+ */
+
